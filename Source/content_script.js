0a020269ffe63bdd957e9c8b148bddc4c73d35c7
Toni Lähdekorpi <toni@lygon.net>
2013-03-14 13:11:15 +0200

    Butt -> Bullshit

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,12 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-    v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-    v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+    v = v.replace(/\bThe Cloud\b/g, "Bullshit");
+	v = v.replace(/\bThe cloud\b/g, "Bullshit");
+    v = v.replace(/\bthe Cloud\b/g, "bullshit");
+	v = v.replace(/\bthe cloud\b/g, "bullshit");
+    v = v.replace(/\bCloud\b/g, "Bullshit");
+	v = v.replace(/\bcloud\b/g, "bullshit");
 	
 	textNode.nodeValue = v;
 }
